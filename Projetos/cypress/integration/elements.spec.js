797745feec746a8016bf23e7fff4b952aa467273
Fabio Passos <61357219+FabioPassos10@users.noreply.github.com>
2024-02-09 15:04:51 -0300

    update

--- Projetos/cypress/integration/elements.spec.js
+++ Projetos/cypress/integration/elements.spec.js
@@ -1,14 +1,35 @@
 describe('Trabalhando com elementos basicos', () => {
-    it('Textos', () => {
+    before(() => { //executa 1 vez antes de todos os testes
         cy.visit('https://wcaquino.me/cypress/componentes.html');
+    });
+    beforeEach(() => {  //executa antes de cada um dos testes
+        cy.reload();
+    });
+    it('Textos', () => {
+        
         cy.get('body').should('contain','Cuidado onde clica, muitas armadilhas...')
         cy.get('span').should('contain','Cuidado onde clica, muitas armadilhas...')
         cy.get('.facilAchar').should('contain','Cuidado onde clica, muitas armadilhas...')
     });
-    it.only('Links', () => {
-        cy.visit('https://wcaquino.me/cypress/componentes.html');
+    it('Links', () => {
+        
         cy.get('[href="#"]').click();
         cy.get('#resultado').should('contain', 'Voltou')
         cy.reload();
+    });
+
+    it('Campos de texto', () => {
+        cy.get('#formNome').type("Fábio Eloy Passos")
+        cy.get('#formNome').should('have.value','Fábio Eloy Passos')
+        cy.get('#elementosForm\\:sugestoes').type("testando se realmente da para escrever um texto grande com muitos caracteres aleatorios neste campo de textarea").should('have','testando se realmente da para escrever um texto grande com muitos caracteres aleatorios neste campo de textarea')
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input').type("Oi")
+        cy.get('[data-cy=dataSobrenome]').type('teste12345678{backspace}{backspace}{backspace}{backspace}{backspace}').should('have.value','teste123')
+        cy.get('[data-cy=dataSobrenome]').clear().type('teste12345678{selectall}teste').should('have.value','teste')
+    });
+
+    it.only('RedioButton', () => {
+        cy.get("[name='formSexo']").should('have.length',2)
+        cy.get('#formSexoMasc').check().should('be.checked')
+        cy.get('#formSexoFem').should('not.to.be.checked')
     });
 });
--- Projetos/cypress/integration/serveRest.spec.js
+++ Projetos/cypress/integration/serveRest.spec.js
@@ -1,9 +1,27 @@
-describe('Cypress basic', () => {
-    it('Visitando uma página e fazendo assertiva no título', () => {
-        cy.visit('https://front.serverest.dev/login');
-        cy.title()
-        .should('be.equal','Front - ServeRest')
-        .should('contain','Rest');
+describe('ServeRest - cypress', () => {
+
+beforeEach(() => {
+    cy.visit('https://front.serverest.dev/login');
+});
+    it('Cenario de cadastro de usuário adm', () => {
+        
+        cy.get('[data-testid=cadastrar]').click();
+        cy.get('[data-testid=nome]').type("UserADM")
+        cy.get('[data-testid=email]').type("userteste@gmail.com")
+        cy.get('[data-testid=password]').type("teste12345")
+        cy.get('[data-testid=checkbox]').check()
+        cy.get('[data-testid=cadastrar]').click()
         
     });
+    it('Cenario de cadastro de usuário comum', () => {
+        
+        cy.get('[data-testid=cadastrar]').click();
+        cy.get('[data-testid=nome]').type("UserComum")
+        cy.get('[data-testid=email]').type("usercomum@gmail.com")
+        cy.get('[data-testid=password]').type("12345678f")
+        cy.get('[data-testid=cadastrar]').click()
+        
+    });
+
+    
 });
